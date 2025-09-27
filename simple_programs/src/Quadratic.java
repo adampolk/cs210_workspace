b063302931b1c49852e831482361faca6b805931
apolk25 <apolk25@hanoverstudents.org>
2025-09-27 18:44:12 -0400

    Fixed issue with checking negative discriminants for Quadratic problem.

--- simple_programs/src/Quadratic.java
+++ simple_programs/src/Quadratic.java
@@ -6,7 +6,7 @@ public class Quadratic {
         double a = Double.parseDouble(args[0]);
         double b = Double.parseDouble(args[1]);
         double c = Double.parseDouble(args[2]);
-        double discriminant = Math.sqrt((b*b - (4 * a * c)));
+        double discriminant = b*b - (4 * a * c);
 
         // Make sure a is not 0 and the discriminant is not negative before solving for zeros
         if(a == 0){
@@ -14,8 +14,8 @@ public class Quadratic {
         }else if(discriminant < 0){
             StdOut.println("Value of discriminant must not be negative.");
         }else{
-            double first_zero = (-b + discriminant) / (2 * a);
-            double second_zero = (-b - discriminant) / (2 * a);
+            double first_zero = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double second_zero = (-b - Math.sqrt(discriminant)) / (2 * a);
             StdOut.println(first_zero + " " + second_zero);
         }
     }
